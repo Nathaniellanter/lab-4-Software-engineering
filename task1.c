ada2b889006f111de69abb0b835e80a373a271fe
Lanter <n431l395@engr1005c-19.home.ku.edu>
2026-09-23 09:40:42 -0500

    input and output works

---
+++ task1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//brute force approach
+int score_makeup() {
+    int user_score =2 ;
+    while (user_score>1)
+{
+printf("Enter the score");
+scanf("%i", &user_score);
+}
+}
+
+int main()
+{
+    score_makeup();
+}
